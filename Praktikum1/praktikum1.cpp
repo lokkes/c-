8416d9ddd9ac24e7404f2a067a79f3c7a6be00e0
lokkes <lokkeshwaran.krishnakumar@stud.htw-dresden.de>
2025-03-25 13:52:19 +0100

    formatierung

--- Praktikum1/praktikum1.cpp
+++ Praktikum1/praktikum1.cpp
@@ -1,40 +1,38 @@
 #include <iostream>
-#include <iomanip> 
+#include <iomanip>
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-int i,j;
+    int i, j;
 
-for(i = 1; i <=4; i++){
-cout << "| dec hex char ";
+    for (i = 1; i <= 4; i++)
+    {
+        cout << "| dec hex char ";
+    }
+    cout << "| ";
+    cout << endl;
+    cout << endl;
 
-}
-cout << "| ";
-cout << endl;
-cout << endl;
+    int kbase = 0;
 
+    for (j = 0; j <= 31; j++)
+    {
 
-int kbase = 0;
+        int k = kbase;
+        for (int count = 0; count <= 3; count++)
+        {
 
+            cout << "|" << " " << setfill('0') << setw(3) << dec << k << "  "
+                 << hex << setw(2) << k << "  "
+                 << right << dec << char((k >= 32 && k <= 126) ? k : '.') << "   ";
+            k += 32;
+        }
 
-for(j = 0; j <= 31; j++){
-
-int k = kbase;
-for(int count = 0; count<=3; count++){
-    
-cout  << "|" << " " << setfill('0') << setw(3) << dec << k << "  " 
-<< hex << setw(2) << k << "  "
-<< right << dec << char((k >= 32 && k <= 126) ? k : '.') << "   ";
-k+=32;
-
-}
-
-kbase++;
-cout << "|" << endl;
-
-}
-return 0;
-
+        kbase++;
+        cout << "|" << endl;
+    }
+    return 0;
 }
